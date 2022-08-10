8afd197f99223b9ee3ae28723587a590bcc1c6ea
Harshal Jain <virajj014@gmail.com>
2022-08-11 00:20:09 +0530

    selectionpartdone

--- src/Components/Intro/AndroidIssue.js
+++ src/Components/Intro/AndroidIssue.js
@@ -114,7 +114,7 @@ function AndroidIssue() {
 
 
 
-                    {selectdevice != null ?
+                    {issuelist != '' ?
                         <button className='nextbtnactive'>
                             <span> Next</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
--- src/Components/Intro/WindowsIssue.js
+++ src/Components/Intro/WindowsIssue.js
@@ -113,8 +113,7 @@ function WindowsIssue() {
                         <></>}
 
 
-
-                    {selectdevice != null ?
+                    {issuelist != '' ?
                         <button className='nextbtnactive'>
                             <span> Next</span>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
